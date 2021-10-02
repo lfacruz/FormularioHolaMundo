6dac918d34286828c784813da3bf633de468bc51
lfacu <lfacu@DESKTOP-25A1I87>
2021-10-02 11:59:36 -0600

    Agregar archivos de proyecto.

---
+++ Data/MvcAnimalesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FormularioHolaMundo.Models;
+
+namespace FormularioHolaMundo.Data
+{
+    public class MvcAnimalesContext : DbContext
+    {
+        public MvcAnimalesContext (DbContextOptions<MvcAnimalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FormularioHolaMundo.Models.Animal> Animal { get; set; }
+    }
+}
---
+++ Migrations/20211002171610_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FormularioHolaMundo.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animal", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animal");
+        }
+    }
+}
